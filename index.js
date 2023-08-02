6717c5504448797f1d641ba77d09f3eddd7a01f9
seokhyeon park <dfbgq346q@gmail.com>
2023-08-02 11:31:05 +0900

    *

--- index.js
+++ index.js
@@ -7,15 +7,24 @@ const token = packageData.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 const headers = {
-  'Accept': 'application/json, text/javascript, */*; q=0.01',
-  'Accept-Encoding': 'gzip, deflate',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+  'Accept-Encoding': 'gzip, deflate, br',
   'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+  'Cache-Control': 'max-age=0',
   'Connection': 'keep-alive',
+  'Cookie': 'PCID=16739430089024372260972; _gcl_au=1.1.1877032182.1684292122; afUserId=e3c00608-412b-445c-8290-069041d50ac3-p; RecentViewTruck=; PERSISTENT_USERTYPE=1; cto_bundle=_knICF9Jbjg2aEJhMjMlMkY0Yk13YVRqVWYxdkp4N0dVTDNTRFdsVkd3aTdMVnJORGklMkZPZ1FUbjRuWDVlJTJGOTI2Y2RQWW9qVjRmZU9NaVNuTCUyRmpMZjBiNVpteGJvRnc3ZUUwMXAwSVNZTUVrV0c2bFFTZTduUExFVjBydjZmJTJCekxHSkxWbmR4eGZ0SUloY2RIT0I1Smh5a0VpRSUyQkElM0QlM0Q; _ga_SX6YBF7MKB=GS1.1.1687513344.9.0.1687513353.0.0.0; RecentViewAllCar=34997673%2C35149390%2C35045880; RecentViewCar=34997673%2C35149390%2C35045880; _ga_BQ7RK9J6BZ=GS1.1.1687753971.13.0.1687753974.57.0.0; _ga=GA1.2.235263365.1673943010; _encar_hostname=http://www.encar.com; _gid=GA1.2.365406252.1690159467; _ga_WY0RWR65ED=GS1.2.1690159466.43.1.1690161898.0.0.0',
   'Host': 'api.encar.com',
-  'Origin': 'http://www.encar.com',
-  'Referer': 'http://www.encar.com/',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+  'Sec-Ch-Ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+  'Sec-Ch-Ua-Mobile': '?0',
+  'Sec-Ch-Ua-Platform': '"Windows"',
+  'Sec-Fetch-Dest': 'document',
+  'Sec-Fetch-Mode': 'navigate',
+  'Sec-Fetch-Site': 'none',
+  'Sec-Fetch-User': '?1',
+  'Upgrade-Insecure-Requests': '1',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
 };
+
 
 let requester = [];
 
@@ -222,6 +231,7 @@ bot.on('message', (msg) => {
     if (msg.text.includes("/add") || msg.text.includes("/ADD")) { functions.add(msg); }
     if (msg.text.includes("/list") || msg.text.includes("/LIST")) { functions.list(msg); }
     if (msg.text.includes("/delete") || msg.text.includes("/DELETE")) { functions.delete(msg); }
+    if (msg.text.includes("/check") || msg.text.includes("/CHECK")) { functions.check(); }
     if (msg.text.includes("/help") || msg.text.includes("/HELP")) { functions.help(msg); }
   } catch (err) {
     bot.sendMessage(chatId, `잘못된 요청입니다. 명령어 및 주소를 다시 확인해주세요.`);
